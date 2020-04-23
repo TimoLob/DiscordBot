7483ab8fd3006e752876885a9d11796f46058b5a
BuildTools <unconfigured@null.spigotmc.org>
2020-04-23 16:44:40 +0200

    zwischenschritte

--- commands/square_and_multiply.js
+++ commands/square_and_multiply.js
@@ -14,10 +14,17 @@ module.exports = {
             mod = args[2];
             exp = y.toString(2);
             value = x;
+            let msg = ""
             for(let i=0;i<exp.length;i++) {
+                msg = "x^${exp.substring(0,i)}0=${value}*${value}";
                 value = value *value % mod;
-                if(exp[i]==="1") {
+                msg += "=${value}";
+                message.channel.send(msg);
+                if(exp[i]=="1") {
+                    msg = "x^${exp.substring(0,i+1)}=${value}*${x}";
                     value = value * x % mod;
+                    msg+="=${value}";
+                    message.channel.send(msg);
                 }
             }
             message.channel.send(value);
