99c300e8e0a53fead91280c0715e8f08f31510e1
BuildTools <unconfigured@null.spigotmc.org>
2020-12-27 19:19:40 +0100

    Task(Services) added

--- commands/split_teams.js
+++ commands/split_teams.js
@@ -6,14 +6,14 @@ module.exports = {
 	usage: '!splitteams <t1> <t2> <t3> ...',
 	hidden: true,
 	execute(message, args) {
-		sender = message.member;
+		let sender = message.member;
         if(!utils.isAdmin(sender)) {
 			return message.reply('Only an admin can use this command.');
 		}
-		msg = "";
-		voiceStateSender = sender.voice;
+		let msg = "";
+		let voiceStateSender = sender.voice;
 
-		srcChannel = voiceStateSender.channel;
+		let srcChannel = voiceStateSender.channel;
 
 		if(!srcChannel)
 			return message.channel.send("You must be in a voice channel to use this command.");
@@ -22,7 +22,7 @@ module.exports = {
 			return message.channel.send("Usage: "+usage);
 		}
 
-		members = srcChannel.members; // Collection
+		let members = srcChannel.members; // Collection
 		let memberArray = [];
 		msg+="Detected people:\n";
 		members.forEach(member => {
@@ -31,15 +31,15 @@ module.exports = {
 		});
 		msg+="----------\n";
 		// Fetch voice channels
-		guildchannelmanager = sender.guild.channels;
+		let guildchannelmanager = sender.guild.channels;
 
-		voicechannels = [];
+		let voicechannels = [];
 		args.forEach(channelName => {
 			voicechannels.push(guildchannelmanager.resolve(channelName));
 		})
 		// Initialize empty Teams
-		numTeams = voicechannels.length;
-		teams = new Array(numTeams);
+		let numTeams = voicechannels.length;
+		let teams = new Array(numTeams);
 		for(let i=0;i<numTeams;i++) {
 			teams[i] = [];
 		}
@@ -53,7 +53,7 @@ module.exports = {
 		for(let teamInd=0;teamInd<numTeams;teamInd++) {
 			msg+="Team #"+teamInd+"\n";
 			teams[teamInd].forEach(member => {
-				voiceState = member.voice;
+				let voiceState = member.voice;
 				console.log("Moving "+member.displayName+" to "+voicechannels[teamInd].id);
 				member.voice.setChannel(voicechannels[teamInd]);
 				msg+=member.displayName+"\n";
---
+++ tasks/tasks.js
@@ -0,0 +1,7 @@
+const {GenshinTask} = require("./getCodes");
+// Genshin Impact primo gem codes download task
+const genshinTask = new GenshinTask(10);
+
+module.exports = {
+    genshinTask
+}
