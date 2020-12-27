8601ab1f78e2341d5dff46040bd97389724b7b90
BuildTools <unconfigured@null.spigotmc.org>
2020-12-27 19:29:29 +0100

    Increased interval

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -1,6 +1,6 @@
 const {GenshinTask} = require("./getCodes");
 // Genshin Impact primo gem codes download task
-const genshinTask = new GenshinTask(10);
+const genshinTask = new GenshinTask(86400);
 
 module.exports = {
     genshinTask
